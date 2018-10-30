09f0ef03440764cc37862d17871960c227f1a785
jevstein <jevstein@163.com>
2018-10-29 20:41:50 -0700

    add file lock

---
+++ examples/IPC/filelock/filelock.c
@@ -0,0 +1,102 @@
+/*
+ *  filelock.c 
+ *  filelock
+ *
+ *  Created by Jevstein on 2018/10/30 9:24.
+ *  Copyright @ 2018year Jevstein. All rights reserved.
+ *
+ *  [概念]:
+ *     文件锁
+ *     1.建议性锁：lockf()、fcntl(), [内核和系统都不适用建议性锁，采用强制性锁的影响很大，每次读写操作都必须检查是否有所存在]
+ *     2.强制性锁：fcntl()
+ *     3.记录锁：文件的同一部分不能同时建立读取锁和写入锁
+ *        1).读取锁（共享锁）
+ *        2).写入锁（排斥锁)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/file.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "lock_set.h"
+
+#define WR_NAME "FILE_LOCK_WR"
+#define RD_NAME "FILE_LOCK_RD"
+
+// 写入锁
+int file_lock_wr()
+{
+	int fd = open(WR_NAME, O_RDWR | O_CREAT, 0644);
+	if (fd < 0)
+	{
+		printf("Open file error!\n");
+		return -1;
+	}
+	//给文件上写入锁
+	lock_set(fd, F_WRLCK);
+	{
+		getchar();
+	}
+	//给文件解锁
+	lock_set(fd, F_UNLCK);
+	getchar();
+	close(fd);
+
+	return 0;
+}
+
+// 读取锁
+int file_lock_rd()
+{
+	int fd = open(RD_NAME, O_RDWR | O_CREAT, 0644);
+	if (fd < 0)
+	{
+		printf("Open file error!\n");
+		return -1;
+	}
+	//给文件上读取锁
+	lock_set(fd, F_RDLCK);
+	getchar();
+	//给文件解锁
+	lock_set(fd, F_UNLCK);
+	getchar();
+	close(fd);
+
+	return 0;
+}
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		printf("usage: file_lock [ -wr | -rd ]\n");
+		return -1;
+	}
+
+	if (strcmp(argv[1], "-wr") == 0)
+	{
+		printf("---------- write lock test ---------\n\n");
+		file_lock_wr();
+	}
+	else if (strcmp(argv[1], "-rd") == 0)
+	{
+		printf("---------- read lock test ---------\n\n");
+		file_lock_rd();
+	}
+	else
+	{
+		printf("usage: file_lock [ -wr | -rd ]\n");
+		return -1;
+	}
+
+
+	printf("---------- the end ---------\n");
+
+	return 0;
+}
+
+
+//compile:
+// $ gcc -o ../../../bin/file_lock filelock.c
---
+++ examples/IPC/filelock/lock_set.h
@@ -0,0 +1,79 @@
+/*
+ *  lockset.c 
+ *  lockset
+ *
+ *  Created by Jevstein on 2018/10/30 11:03.
+ *  Copyright @ 2018year Jevstein. All rights reserved.
+ *
+ *  [概念]:
+ *     文件锁
+ *     1.建议性锁：lockf()、fcntl(), [内核和系统都不适用建议性锁，采用强制性锁的影响很大，每次读写操作都必须检查是否有所存在]
+ *     2.强制性锁：fcntl()
+ *     3.记录锁：文件的同一部分不能同时建立读取锁和写入锁
+ *        1).读取锁（共享锁）
+ *        2).写入锁（排斥锁)
+ *  [核心实现]:
+ *      #include <unistd.h>
+ *      #include <fcntl.h>
+ *      int fcntl(int fd, int cmd, ... arg );
+ *      参数1：
+ *      参数2：
+ *         F_SETLK(struct flock *);//设置文件锁 ( trylock )
+ *         F_SETLKW(struct flock*);//设置文件锁（ lock ） W --- wait 
+ *         F_GETLK(struct flock*);//获取文件锁
+ *      参数3：
+ *         struct flock {
+ *             ...
+ *             short l_type;  // Type of lock: F_RDLCK,F_WRLCK, F_UNLCK
+ *             short l_whence;// How to interpret l_start: SEEK_SET, SEEK_CUR, SEEK_END
+ *             off_t l_start; // Starting offset for lock
+ *             off_t l_len;   // Number of bytes to lock
+ *             pid_t l_pid;   // PID of process blocking our lock (F_GETLK only)
+ *             ...
+ *         };
+ */
+
+#include <unistd.h>
+#include <fcntl.h>
+
+int lock_set(int fd, int type)
+{
+	struct flock old_lock, lock;
+	lock.l_whence = SEEK_SET;
+	lock.l_start = 0;
+	lock.l_len = 0;
+	lock.l_type = type;
+	lock.l_pid = - 1;
+
+	//判断文件是否上锁
+	fcntl(fd, F_GETLK, &lock);
+	if (lock.l_type != F_UNLCK)
+	{//判断文件不能上锁的原因
+		switch (lock.l_type)
+		{
+		case F_RDLCK: printf("Read lock already set by %d .\n", lock.l_pid); break;	//该文件已有读取锁
+		case F_WRLCK: printf("Write lock already set by %d .\n", lock.l_pid); break;//该文件已有写入锁
+		default:
+			break;
+		}
+	}
+	//l_type 可能已被F_FETLK修改过
+	lock.l_type = type;
+
+	//根据不同的type值进行阻塞式上锁或解锁
+	if ((fcntl(fd, F_SETLKW, &lock)) < 0)
+	{
+		printf("Lock failed:type = %d\n", lock.l_type);
+		return 1;
+	}
+
+	switch (lock.l_type)
+	{
+	case F_RDLCK: printf("Read lock set by %d {\n", getpid());	break;
+	case F_WRLCK: printf("Write lock set by %d {\n", getpid());	break;
+	case F_UNLCK: printf("} Release lock by %d \n\n", getpid());	return 1;
+	default:
+		break;
+	}
+	return 0;
+}
