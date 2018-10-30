0acf369e3638371148bb1f7e7885047eba8cc79f
jevstein <jevstein@163.com>
2018-10-29 20:46:07 -0700

    add file lock

--- examples/IPC/filelock/lock_set.h
+++ examples/IPC/filelock/lock_set.h
@@ -41,7 +41,7 @@ int lock_set(int fd, int type)
 	struct flock old_lock, lock;
 	lock.l_whence = SEEK_SET;
 	lock.l_start = 0;
-	lock.l_len = 0;
+	lock.l_len = 0; //0 表示对整个文件加锁
 	lock.l_type = type;
 	lock.l_pid = - 1;
 
