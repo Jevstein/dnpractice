320c9cf405daf6dda83349540365881feac0b05e
Jevstein <jevstein@JevsteinMac.local>
2019-01-27 14:01:34 +0800

    add stack and queue

--- homework/04.手写二叉树与红黑树/jvt_btree.c
+++ homework/04.手写二叉树与红黑树/jvt_btree.c
@@ -1,4 +1,13 @@
 
+/*
+ *  jvt_btree.h 
+ *  jvt_btree
+ *
+ *  Created by Jevstein on 2019/01/27 19:36.
+ *  Copyright @ 2019year Jevstein. All rights reserved.
+ *
+ *  二叉排序树：增、删、改、查
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,28 +23,28 @@ struct name {                   \
 }
 
 // 节点
-typedef struct _bstree_node {
+typedef struct _jvt_bstree_node {
     KEY_TYPE data;
 
-    BSTREE_ENTRY(, _bstree_node);
+    BSTREE_ENTRY(, _jvt_bstree_node);
     // BSTREE_ENTRY(Next, bstree_node) next;
-} bstree_node_t;
+} jvt_bstree_node_t;
 
 // 树
-typedef struct _bstree {
-    bstree_node_t *root;
-} bstree_t;
+typedef struct jvt_bstree {
+    jvt_bstree_node_t *root;
+} jvt_bstree_t;
 
-bstree_node_t* bstree_create_node(KEY_TYPE data);
-void bstree_destroy_node(bstree_node_t* node);
-int bstree_traversal(bstree_node_t *node);
-int bstree_insert(bstree_t *T, KEY_TYPE data);                              //增
-int bstree_delete(bstree_t *T, KEY_TYPE data);                              //删
-int bstree_update(bstree_t *T, const KEY_TYPE src, const KEY_TYPE dest);    //改
-bstree_node_t* bstree_find(bstree_t *T, KEY_TYPE data);                     //查
+jvt_bstree_node_t* jvt_bstree_create_node(KEY_TYPE data);
+void jvt_bstree_destroy_node(jvt_bstree_node_t* node);
+int jvt_bstree_traversal(jvt_bstree_node_t *node);
+int jvt_bstree_insert(jvt_bstree_t *T, KEY_TYPE data);                              //增
+int jvt_bstree_delete(jvt_bstree_t *T, KEY_TYPE data);                              //删
+int jvt_bstree_update(jvt_bstree_t *T, const KEY_TYPE src, const KEY_TYPE dest);    //改
+jvt_bstree_node_t* jvt_bstree_find(jvt_bstree_t *T, KEY_TYPE data);                     //查
 
-bstree_node_t* bstree_create_node(KEY_TYPE data) {
-    bstree_node_t* node = (bstree_node_t*)calloc(1, sizeof(bstree_node_t));
+jvt_bstree_node_t* jvt_bstree_create_node(KEY_TYPE data) {
+    jvt_bstree_node_t* node = (jvt_bstree_node_t*)calloc(1, sizeof(jvt_bstree_node_t));
     assert(node);//若连分配内存不成功，其它一定也可能出现异常，此时最适合用断言
 
     node->data = data;
@@ -43,7 +52,7 @@ bstree_node_t* bstree_create_node(KEY_TYPE data) {
     return node;
 }
 
-void bstree_destroy_node(bstree_node_t* node){
+void jvt_bstree_destroy_node(jvt_bstree_node_t* node){
     if (!node)
         return;
 
@@ -51,29 +60,29 @@ void bstree_destroy_node(bstree_node_t* node){
     free(node);
 }
 
-int bstree_traversal(bstree_node_t *node) {
+int jvt_bstree_traversal(jvt_bstree_node_t *node) {
     if (!node)
         return -1;
 
 	// printf("%4d", node->data);//置此：前序遍历
-    bstree_traversal(node->left);
+    jvt_bstree_traversal(node->left);
 	printf("%4d", node->data);//置此：中序遍历
-	bstree_traversal(node->right);
+	jvt_bstree_traversal(node->right);
 	// printf("%4d", node->data);//置此：后序遍历
 
     return 0;
 }
 
-int bstree_insert(bstree_t *T, KEY_TYPE data){
+int jvt_bstree_insert(jvt_bstree_t *T, KEY_TYPE data){
     assert(T);
 
     if (!T->root) {
-        T->root = bstree_create_node(data);
+        T->root = jvt_bstree_create_node(data);
         return 0;
     }
 
-    bstree_node_t* parrent = T->root;
-    bstree_node_t* p = T->root;
+    jvt_bstree_node_t* parrent = T->root;
+    jvt_bstree_node_t* p = T->root;
 
     while (p) {
         parrent = p;
@@ -81,20 +90,20 @@ int bstree_insert(bstree_t *T, KEY_TYPE data){
     }
 
     if (data < parrent->data)
-        parrent->left = bstree_create_node(data);
+        parrent->left = jvt_bstree_create_node(data);
     else 
-        parrent->right = bstree_create_node(data);
+        parrent->right = jvt_bstree_create_node(data);
 
     return 0;
 }
 
-int bstree_delete(bstree_t *T, KEY_TYPE data) {
+int jvt_bstree_delete(jvt_bstree_t *T, KEY_TYPE data) {
     assert(T);
 
-    bstree_node_t* p = T->root;
-    bstree_node_t* temp = NULL;
-    bstree_node_t* parent = NULL;
-    bstree_node_t* node = NULL;
+    jvt_bstree_node_t* p = T->root;
+    jvt_bstree_node_t* temp = NULL;
+    jvt_bstree_node_t* parent = NULL;
+    jvt_bstree_node_t* node = NULL;
 
     // 找出待删节点, 和它的父节点
     while(p) {
@@ -167,12 +176,12 @@ int bstree_delete(bstree_t *T, KEY_TYPE data) {
         }
     }
     
-    bstree_destroy_node(node);
+    jvt_bstree_destroy_node(node);
 
     return 0;
 }
 
-int bstree_update(bstree_t *T, const KEY_TYPE src, const KEY_TYPE dest) {
+int jvt_bstree_update(jvt_bstree_t *T, const KEY_TYPE src, const KEY_TYPE dest) {
     //分为以下步骤:
     // 1.找出值为src的源节点node*，并将值替换为dest
     // 2.赋予新值后检查是否有序，若有序则返回；若无序则移除node*节点，并重新保证二叉排序依然有序
@@ -183,10 +192,10 @@ int bstree_update(bstree_t *T, const KEY_TYPE src, const KEY_TYPE dest) {
     if (src == dest)
         return 0;
 
-    bstree_node_t* parent = NULL;
-    bstree_node_t* node = T->root;
-    bstree_node_t* p = NULL;
-    bstree_node_t* s = NULL;//p的父节点
+    jvt_bstree_node_t* parent = NULL;
+    jvt_bstree_node_t* node = T->root;
+    jvt_bstree_node_t* p = NULL;
+    jvt_bstree_node_t* s = NULL;//p的父节点
 
     //1.查找&赋值
     while (node) {
@@ -279,10 +288,10 @@ int bstree_update(bstree_t *T, const KEY_TYPE src, const KEY_TYPE dest) {
     return 0;
 }
 
-bstree_node_t* bstree_find(bstree_t *T, KEY_TYPE data){
+jvt_bstree_node_t* jvt_bstree_find(jvt_bstree_t *T, KEY_TYPE data){
     assert(T);
 
-    bstree_node_t* node = T->root;
+    jvt_bstree_node_t* node = T->root;
     
     while (node) {
         if (node->data == data)
@@ -299,17 +308,17 @@ bstree_node_t* bstree_find(bstree_t *T, KEY_TYPE data){
 int main () {
     KEY_TYPE data_array[ARRAY_LENGTH] = {89, 45, 56, 32, 41, 90, 21, 43, 87, 76};
 
-    bstree_t T = {0};
+    jvt_bstree_t T = {0};
     int i = 0;
     int data = 0;
-    bstree_node_t *node = NULL;
+    jvt_bstree_node_t *node = NULL;
 
     // 增
     printf("========= insert =========\n");
     for (i = 0; i < ARRAY_LENGTH; ++i) {
         printf("insert[%d][%d]: ", i, data_array[i]);
-        bstree_insert(&T, data_array[i]);
-        bstree_traversal(T.root);
+        jvt_bstree_insert(&T, data_array[i]);
+        jvt_bstree_traversal(T.root);
         printf("\n");
     }
 
@@ -318,8 +327,8 @@ int main () {
     for (i = 0; i < ARRAY_LENGTH; ++i) {
         data = (i % 2 == 0) ? data_array[i] + 10 : data_array[i] - 10;
         printf("update[%d][%d->%d]: ", i, data_array[i], data);
-        bstree_update(&T, data_array[i], data);
-        bstree_traversal(T.root);
+        jvt_bstree_update(&T, data_array[i], data);
+        jvt_bstree_traversal(T.root);
         printf("\n");
         data_array[i] = data;
     }
@@ -331,7 +340,7 @@ int main () {
         if ( i != -1)
             data = data_array[i];
 
-        node = bstree_find(&T, data);
+        node = jvt_bstree_find(&T, data);
         if (node)
             printf("find[%d][%d][%p]: data=%d, left=%p, right=%p\n", i, data, node, node->data, node->left, node->right);
         else
@@ -342,8 +351,11 @@ int main () {
     printf("========= delete =========\n");
     for (i = 0; i < ARRAY_LENGTH; ++i) {
         printf("delete[%d][%d]: ", i, data_array[i]);
-        bstree_delete(&T, data_array[i]);
-        bstree_traversal(T.root);
+        jvt_bstree_delete(&T, data_array[i]);
+        jvt_bstree_traversal(T.root);
         printf("\n");
     }
+
+    printf("========= the end =========\n");
+    printf("T.root: %p\n", T.root);
 }
---
+++ homework/04.手写二叉树与红黑树/rbtree.c
@@ -0,0 +1,14 @@
+/*
+ *  rbtree.h 
+ *  rbtree
+ *
+ *  Created by Jevstein on 2019/01/27 19:36.
+ *  Copyright @ 2019year Jevstein. All rights reserved.
+ *
+ *  红黑树：增、删、改、查
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
